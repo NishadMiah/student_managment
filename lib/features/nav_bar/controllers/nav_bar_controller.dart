19956db9a3f9bebdaff11d668ac9226ed6176ec5
tonim <asiftonim1000@gmail.com>
2025-12-02 09:07:23 +0600

    Co-authored-by: asiftonim <asiftonim@users.noreply.github.com> ``` :contentReference[oaicite:5]{index=5}

--- lib/features/nav_bar/controllers/nav_bar_controller.dart
+++ lib/features/nav_bar/controllers/nav_bar_controller.dart
@@ -7,12 +7,6 @@ import '../../profile/presentations/screens/profile_screen.dart';
 class NavBarController extends GetxController {
   var currentIndex = 0.obs;
 
-  // Center(child: HomeScreen()),
-  // Center(child: CalenderScreen()),
-  // Center(child: ChatScreen()),
-  // Center(child: ProfileScreen()),
-  // ];
-  // Pages list
   final pages = [
 
      Center(child: HomeScreen()),
